9fe8a7080b3fa3e816db1af7920fc28298254ce8
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-18 21:59:03 +0100

    modify script.js

--- script.js
+++ script.js
@@ -11,12 +11,42 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  document.querySelector('.message').textContent = '⛔️ No number!';
+  //When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+
+    //when Player Wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+
+    //When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--; //same as score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too Low!';
+      score--; //same as score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
