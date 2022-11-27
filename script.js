df9b7932f91e0dce2d0573e156d77712da4ec359
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-11-27 20:54:40 +0530

    modify script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ document.querySelector('.check').addEventListener('click', function () {
     if (score > 1) {
       // document.querySelector('.message').textContent =
       //   guess > secretNumber ? '📈 Too high!' : '📈 Too Low!';
-      displayMessage(guess > secretNumber ? '📈 Too high!' : '📈 Too Low!');
+      displayMessage(guess > secretNumber ? '📈 Too low!' : '📈 Too high!');
       score--; //same as score = score - 1;
       document.querySelector('.score').textContent = score;
     } else {
